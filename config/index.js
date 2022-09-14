1ca7a7fe255e1c3923544b6459284c1d87c11fb5
Ktwo <58912150+1scy@users.noreply.github.com>
2022-09-14 17:15:53 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -25,7 +25,7 @@ export const config = {
       // 想要发送的人的名字
       name: "芸宝宝",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "olQY26kJ9EWhpH_Z_UjomiJOvtek",
+      id: ["olQY26kJ9EWhpH_Z_UjomiJOvtek","olQY26qdFfvOfFQNWFu9yLY3b-Kw"],
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "pXulAYFi9C4NQ6q3X65H4OXJR1rR10b2o6Xbks-LFlY",
       // 所在省份
@@ -57,42 +57,7 @@ export const config = {
         {"keyword": "ex_day", date: "2022-09-10"},
       ]
     },
-    {
-      // 想要发送的人的名字
-      name: "铭宝宝",
-      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "olQY26qdFfvOfFQNWFu9yLY3b-Kw",
-      // 你想对他发送的模板消息的模板ID
-      useTemplateId: "pXulAYFi9C4NQ6q3X65H4OXJR1rR10b2o6Xbks-LFlY",
-      // 所在省份
-      province: "广东",
-      // 所在城市
-      city: "广州",
-      // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '04-1',
-      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
-      horoscopeDateType: '今日',
-      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "#",
-      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "苏伟铭", "year": "2000", "date": "05-23"},
-        {"type": "节日", "name": "周年纪念日", "year": "2020", "date": "02-14"},
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {"type": "*生日", "name": "陈芸洋", "year": "2000", "date": "02-27"},
-//         {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
-      ],
-      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
-      customizedDateList: [
-        // 在一起的日子
-        {"keyword": "love_day", date: "2020-02-14"},
-        // 结婚纪念日
-        {"keyword": "marry_day", date: "2022-09-09"},
-        // 退伍日
-        {"keyword": "ex_day", date: "2022-09-10"},
-      ]
-    },
+   
     {
       name: "老婆1",
       id: "",
