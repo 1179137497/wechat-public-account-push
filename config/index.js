23fb6e975feaa17860a5a3565677cb32ab0963d5
Ktwo <58912150+1scy@users.noreply.github.com>
2022-09-14 16:28:19 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -59,7 +59,7 @@ export const config = {
     },
     {
       // 想要发送的人的名字
-      name: "芸宝宝",
+      name: "铭宝宝",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "olQY26qdFfvOfFQNWFu9yLY3b-Kw",
       // 你想对他发送的模板消息的模板ID
